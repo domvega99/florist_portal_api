3207afc378cf14160f9c1566a78a9fe95ad9b0af
domvega99 <davega12.dv@gmail.com>
2025-04-16 16:23:35 +0800

    Florist API

--- app/Http/Controllers/FloristController.php
+++ app/Http/Controllers/FloristController.php
@@ -5,62 +5,101 @@ namespace App\Http\Controllers;
 use App\Models\Florist;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class FloristController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        return 'Florist Test';
+        $query = Florist::query();
+        // Inputs
+        $search = $request->input('search');
+        $sort = $request->input('sort', 'id');
+        $order = $request->input('order', 'asc');
+        $page = $request->input('page', 0);
+        $perPage = $request->input('per_page', 20);
+        $status = $request->input('status');
+        $city = $request->input('city');
+        $floristRep = $request->input('florist_rep');
+
+
+        // Search
+        if ($request->has('search')) {
+            $columns = Schema::getColumnListing((new Florist)->getTable());
+            $query->where(function ($q) use ($columns, $search) {
+                foreach ($columns as $column) {
+                    $q->orWhere($column, 'like', '%' . $search . '%');
+                }
+            });
+        }
+
+        // Filters
+        if ($request->filled('status')) {
+            $query->where('status', $status);
+        }
+        if ($request->filled('city')) {
+            $query->where('city', $city);
+        }
+        if ($request->filled('florist_rep')) {
+            $query->where('florist_rep', $floristRep);
+        }
+
+        // Sorting
+        if (!in_array(strtolower($order), ['asc', 'desc'])) {
+            $order = 'asc';
+        }
+        $query->orderBy($sort, $order);
+
+        // Pagination
+        $tasks = $query->paginate($perPage, ['*'], 'page', $page + 1);
+
+        // Response
+        return response()->json([
+            'per_page' => $tasks->perPage(),
+            'total' => $tasks->total(),
+            'data' => $tasks->items(),
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request = $request->validate([
+            'florist_name' => 'required',
+            'contact_number' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+        ]);
+
+        $florist = Florist::create($request);
+
+        return $florist;
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Florist $florist)
     {
-        //
+        return $florist;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Florist $florist)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Florist $florist)
     {
-        //
+        $request = $request->validate([
+            'florist_name' => 'required',
+            'contact_number' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+        ]);
+
+        $florist->update($request);
+
+        return $florist;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Florist $florist)
     {
-        //
+        $florist->delete();
+
+        return [
+            'message' => 'Florist deleted successfully'
+        ];
     }
 }
--- app/Models/Florist.php
+++ app/Models/Florist.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Florist extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'florist_name',
+        'contact_number',
+        'address',
+        'city',
+        'postcode',
+        'delivery_fee',
+        'sell_extras',
+    ];
+
+    public $timestamps = false;
 }
